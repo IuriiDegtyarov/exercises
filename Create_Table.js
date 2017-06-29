7e507fe94e56f3ebe37f611bb5af0643e592fdcd
IuriiDegtyarov <degtyarev.yuri.v@gmail.com>
2017-06-29 14:05:06 +0300

    exercises_CreateTable

---
+++ Create_Table.js
@@ -0,0 +1,21 @@
+function createTable(rows, columns){
+  const table = document.createElement('table');
+  let row;
+  table.setAttribute('border', '1px');
+  table.style = 'text-align: center';
+  for (let i = 0; i < rows; i++) {
+	  table.appendChild(document.createElement('tr')); 	
+  }
+  for (let j = 0; j < columns; j++) {
+    if (i = 0){
+  	  const headerCell = document.createElement('th');
+  	  row = table.rows[i];
+  	  row.appendChild(headerCell);
+  	}else{
+  	  const cell = document.createElement('td');
+  	  row = table.rows[i];
+  	  row.appendChild(cell);
+  	}
+  }
+  document.body.appendChild(table);
+}
